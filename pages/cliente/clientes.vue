452f9405501234a9a03d015959feaefd6fbf4528
nunso478 <nunso478@gmail.com>
2021-04-07 19:31:50 +0100

    novo

---
+++ pages/cliente/clientes.vue
@@ -0,0 +1,6 @@
+<template>
+    <div>
+    <h1>clientes novos</h1>
+    <nuxt-link to="/">home </nuxt-link>
+    </div>
+</template>
---
+++ pages/users.vue
@@ -0,0 +1,7 @@
+<template>
+    <div>
+    <h1>use</h1>
+    <nuxt-link to="/">home </nuxt-link>
+    </div>
+    
+</template>
